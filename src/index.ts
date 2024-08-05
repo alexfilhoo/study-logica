766930087d9b3d9972a5abbb5b4f30508f52f789
Alexandro Filho <alexandro.filho@pullse.online>
2024-08-05 11:23:25 -0300

    feat: last updates

--- src/index.ts
+++ src/index.ts
@@ -34,3 +34,38 @@ import promptSync from 'prompt-sync';
 //   console.log("The coordinate's y value is " + pt.y);
 // }
 // printCoord({ x: 3, y: 7 });
+
+/// trigesimo segundo vetor ou matriz
+// var vetor: number[] = [1, 2, 3, 4, 5, 30, 70];
+// vetor[0] = 13
+// vetor.push(10);
+// console.log(vetor);
+
+// var matriz: number[][] = [[],[]];
+// matriz[0][0] = 10;
+// matriz[0][1] = 15;
+// matriz[1][0] = 30;
+// matriz[1][1] = 35;
+// matriz.push([10, 20]);
+// matriz[2].push(80);
+
+// console.log(matriz);
+
+// for (var i:number = 0; i < vetor.length; i++) {
+//     console.log(vetor[i]);
+// }
+
+///trigesimo terceiro imprimindo matrizes
+// for (var hora: number = 0; hora < 24; hora++) {
+//     for (var minuto: number = 0; minuto < 60; minuto++) {
+//             console.log(hora + ":" + minuto);
+//     }
+// }
+
+///trigesimo quarto operacoes vetores
+const prompt = promptSync();
+
+for(var i: number = 0; i < 5; i++) {
+    const peso: number = parseInt (prompt("Digite o peso do aluno: "));
+    
+}
